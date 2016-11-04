bbf33f352c09a6f7c528f5e958d85248f30f6ae0
Travis Thurber <travis.thurber@seattlechildrens.org>
2016-11-04 12:54:02 -0700

    support random data in more than 2 dimensions; update instructions

--- src/containers/DataLoader.js
+++ src/containers/DataLoader.js
@@ -12,8 +12,14 @@ const mapStateToProps = (state, ownProps) => ({
 const mapDispatchToProps = (dispatch, ownProps) => ({
   getRandomData: () => {
     let N = getRandomUniform(500, 1000, true)
-    let k = getRandomUniform(3, 6, true)
-    dispatch(Actions.setData(List(getRandomClusters(N, k))))
+    let k = getRandomUniform(3, 9, true)
+    let d = getRandomUniform(2, 5, true)
+    let H = []
+    for (let i = 0; i < d; i++) {
+      H.push('abcdefghijklmnopqrstuvwxyz'[i])
+    }
+    dispatch(Actions.setHeaders(List(H)))
+    dispatch(Actions.setData(List(getRandomClusters(N, k, d))))
   },
   loadData: (H, D) => {
     dispatch(Actions.setHeaders(List(H)))
--- src/methods/getRandomClusters.js
+++ src/methods/getRandomClusters.js
@@ -1,19 +1,26 @@
 import getRandomUniform from './getRandomUniform'
 import getRandomNormal from './getRandomNormal'
 
-export default ( N, k ) => {
+export default ( N, k, d = 2 ) => {
   const n = N / k
   let X = [ ]
   for ( let i = 0 ; i < k ; i++ ) {
-    let c = [ getRandomUniform( -1, 1 ), getRandomUniform( -1, 1 ) ]
+    let c = [ ]
+    for ( let j = 0 ; j < d ; j++ ) {
+      c.push( getRandomUniform( -1, 1 ) )
+    }
     let s = getRandomUniform( 0.05, 0.5 )
     let x = [ ]
     while ( x.length < n ) {
-      let a = getRandomNormal( c[0], s )
-      let b = getRandomNormal( c[1], s )
-      if ( Math.abs( a ) < 1 && Math.abs( b ) < 1 ) {
-        x.push([ a, b ])
+      let p = [ ]
+      for ( let j = 0 ; j < d ; j++ ) {
+        let a = getRandomNormal( c[j], s )
+        while ( Math.abs( a ) >= 1) {
+          a = getRandomNormal( c[j], s )
+        }
+        p.push( a )
       }
+      x.push( p )
     }
     X = X.concat( x )
   }
